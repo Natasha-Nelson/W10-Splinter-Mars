e865bea39f8b4cc38e115b154e67ae8f16d09300
Natasha Nelson <natasha.leah.nelson@gmail.com>
2021-06-26 22:40:24 -0400

    Updating scraping file to connect with MongoDB

--- scraping.py
+++ scraping.py
@@ -4,9 +4,28 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    #Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    # Set variables to capture results from mars_news
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+    
+    #Stop webdriver and return data
+    browser.quit()
+    return data
 
 def mars_news(browser):
 
@@ -32,7 +51,6 @@ def mars_news(browser):
         return None, None
 
     # Use the return function to store the news title and paragraph
-    browser.quit()
     return news_title, news_p
 
 #### Featured Images
@@ -76,6 +94,8 @@ def mars_facts():
     df.columns=['Description','Mars','Earth']
     df.set_index('Description', inplace=True)
 
-    browser.quit()
+    return df.to_html(classes="table table-striped")
 
-    return df.to_html()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
