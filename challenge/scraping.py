5f919904c561b9e64895bb4ef79c4ec2f070d49d
Natasha Nelson <natasha.leah.nelson@gmail.com>
2021-06-27 16:24:20 -0400

    Updating files to change scraping methodology to visit each link, grab the jpg file and update accordingly

--- challenge/scraping.py
+++ challenge/scraping.py
@@ -20,7 +20,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemispheres(browser)
     }
     
     #Stop webdriver and return data
@@ -80,7 +81,6 @@ def featured_image(browser):
     # Use the base URL to create an absolute URL
     img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-    browser.quit()
     return img_url
 
 
@@ -99,3 +99,36 @@ def mars_facts():
 if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
+
+### Hemisphere image and title scrape
+def hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    html = browser.html
+    hemisphere_soup = soup(html, 'html.parser')
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    ## 3a. Create a list that contains the title/image content for each of the 4 products on the page
+    container = hemisphere_soup.find('div', class_='collapsible results')
+    urls = container.find_all('div', class_='item')
+
+    ## 3b. Loop through the list to scrape the specific data points into a dictionary which is added to the list created earlier
+    try:
+        for url in range(len(urls)):
+            browser.find_by_css('a.product-item img')[url].click()
+            sample_elem = browser.links.find_by_text('Sample').first
+            img_url = sample_elem['href']
+            title = browser.find_by_css("h2.title").text
+            hemisphere_image_urls.append ({"img_url": img_url,
+                                                "title": title})
+            browser.back()
+    except AttributeError:
+        return None
+    
+    return hemisphere_image_urls
+    
